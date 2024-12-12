248e3f53695d17fb436366761ac409910b57fcf0
amin <aminkhm097@gmail.com>
2024-12-12 18:56:09 +0330

    feat: add Object model

--- app/web/admin.py
+++ app/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from web.models import Object
+
 # Register your models here.
+
+
+@admin.register(Object)
+class ObjectAdmin(admin.ModelAdmin):
+    model = Object
+    list_display = ['owner' , 'name' , 'type' , 'size']
--- app/web/models.py
+++ app/web/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
 from django.db import models
 
+
+
 # Create your models here.
+def upload_to(instance, filename):
+    return f'drive/{instance.owner.username}/{filename}'
+
+
+
+class Object(models.Model):
+    TYPE_CHOICES = {
+        'folder': 'Folder',
+        'file': 'File',
+    }
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE  , related_name='owner')
+    name = models.CharField(max_length=150, verbose_name='Name')
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, verbose_name='Type')
+    uploadfile = models.FileField(upload_to=upload_to, verbose_name='File' , null=True, blank=True)
+    size = models.PositiveIntegerField(verbose_name='Size' , null=True, blank=True)
+    path = models.TextField(verbose_name='Path')
+    trash = models.BooleanField(default=False, verbose_name='Is Trash')
+    stared = models.BooleanField(default=False, verbose_name='Is Stared')
+    shared = models.BooleanField(default=False, verbose_name='Is Shared')
+    shared_link = models.CharField(max_length=200, null=True, blank=True, verbose_name='Shared Link')
+    shared_to = models.ManyToManyField(settings.AUTH_USER_MODEL , related_name='shared' , verbose_name='Shared To' , blank=True , null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated At')
+
+    def __str__(self):
+        return f'{self.owner} - {self.name}'
+
+    def save(self, *args, **kwargs):
+        if self.uploadfile:
+            self.size = self.uploadfile.size / 1024
+        super().save(*args, **kwargs)
