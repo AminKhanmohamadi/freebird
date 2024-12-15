4addc9ecf47a5590b5b6af2d902de7f3edca6200
amin <aminkhm097@gmail.com>
2024-12-15 23:11:03 +0330

    feat: add setcookie function

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -36,3 +36,9 @@ function humanFileSize(size) {
 }
 
 
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
